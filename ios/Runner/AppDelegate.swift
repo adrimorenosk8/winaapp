f957ccf7648599af02001ac781741a3d7c76be38
adrimorenosk8 <adrimorenosk8@gmail.com>
2025-09-15 20:53:10 +0200

    Revert project to old working version12

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +10,42 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Inicializa Firebase
+    FirebaseApp.configure()
+    
+    // Configura notificaciones push
+    UNUserNotificationCenter.current().delegate = self
+    
+    // Solicita permisos al usuario
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: [.alert, .badge, .sound]
+    ) { granted, error in
+      if let error = error {
+        print("Error solicitando permisos: \(error)")
+      } else {
+        print("Permisos concedidos: \(granted)")
+      }
+    }
+    
+    // Registro en APNs
+    application.registerForRemoteNotifications()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // MARK: - APNs Token registrado
+  override func application(_ application: UIApplication,
+                            didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+    print("APNs device token: \(deviceToken.map { String(format: "%02.2hhx", $0) }.joined())")
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+
+  // MARK: - Error al registrar en APNs
+  override func application(_ application: UIApplication,
+                            didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("Error registrando APNs: \(error)")
+    super.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
   }
 }
