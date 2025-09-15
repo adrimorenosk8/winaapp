21b203c78c84201742bc6069c5c5598a8a9a5210
adrimorenosk8 <adrimorenosk8@gmail.com>
2025-09-15 22:39:40 +0200

    Revert project to old working version15

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
-import UIKit
 import Flutter
-import FirebaseCore
-import FirebaseMessaging
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,27 +7,7 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    // Inicializa Firebase solo si no estaba inicializado
-    if FirebaseApp.app() == nil {
-      FirebaseApp.configure()
-    }
-
-    // 🔹 Registra plugins de Flutter
     GeneratedPluginRegistrant.register(with: self)
-
-    // 🔹 Configura Firebase Messaging para APNs
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // MARK: - APNs Token registrado
-  override func application(_ application: UIApplication,
-                            didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-    print("📱 APNs token registrado correctamente")
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
